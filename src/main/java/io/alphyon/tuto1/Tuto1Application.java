3005e452c426639cc174ed1b24b5a75b4185b023
José Antonio Henríquez Chavarría <alphyon21@gmail.com>
2018-02-05 17:21:00 -0600

    add jpa

---
+++ src/main/java/io/alphyon/tuto1/Tuto1Application.java
@@ -0,0 +1,13 @@
+package io.alphyon.tuto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuto1Application.class, args);
+
+	}
+}
---
+++ src/test/java/io/alphyon/tuto1/Tuto1ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.alphyon.tuto1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tuto1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
